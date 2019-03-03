95271d17e2ebab4975c6beb0892cbb858785666e
Lukasz Bezlada <bezi@o2.pl>
2019-03-03 18:27:14 +0100

    Tests with Exceptions

--- src/main/java/com/infoshareacademy/junit/task2/FizzBuzz.java
+++ src/main/java/com/infoshareacademy/junit/task2/FizzBuzz.java
@@ -2,6 +2,11 @@ package com.infoshareacademy.junit.task2;
 
 public class FizzBuzz {
     public String play(int number) {
+
+        if (number <= 0) {
+            throw new ArithmeticException("Number can not be 0 or negative!");
+        }
+
         if (number % 3 == 0 && number % 5 != 0)
             return "Fizz!";
         else if (number % 5 == 0 && number % 3 != 0)
--- src/test/java/com/infoshareacademy/junit/task2/FizzBuzzTest.java
+++ src/test/java/com/infoshareacademy/junit/task2/FizzBuzzTest.java
@@ -1,40 +1,59 @@
 package com.infoshareacademy.junit.task2;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class FizzBuzzTest {
     private FizzBuzz fizzBuzz;
 
     @BeforeEach
-    public void init() {
+    void init() {
         fizzBuzz = new FizzBuzz();
     }
 
     @Test
-    public void shouldReturnFizz() {
+    @DisplayName("Should return Fizz! if number is divisible by 3")
+    void shouldReturnFizz() {
         String result = fizzBuzz.play(9);
         assertEquals(result, "Fizz!");
     }
 
     @Test
-    public void shouldReturnBuzz() {
+    @DisplayName("Should return Buzz! if number is divisible by 5")
+    void shouldReturnBuzz() {
         String result = fizzBuzz.play(20);
         assertEquals(result, "Buzz!");
     }
 
     @Test
-    public void shouldReturnFizzBuzz() {
+    @DisplayName("Should return FizzBuzz! if number is divisible by 5 and 3")
+    void shouldReturnFizzBuzz() {
         String result = fizzBuzz.play(15);
         assertEquals(result, "FizzBuzz!");
     }
 
     @Test
-    public void shouldReturnNumber() {
+    @DisplayName("Should return inserted number if number is not divisible by 3 or 5")
+    void shouldReturnNumber() {
         String result = fizzBuzz.play(16);
         assertEquals(result, "16");
     }
+
+    @Test
+    @DisplayName("Should throw exception if number = 0")
+    public void shouldThrowExceptionIfNumberIsZero() {
+        assertThrows(ArithmeticException.class, () -> fizzBuzz.play(0));
+    }
+
+    @Test
+    @DisplayName("Should throw exception if number < 0")
+    public void shouldThrowExceptionIfNumberIsLessThanZero() {
+        assertThrows(ArithmeticException.class, () -> fizzBuzz.play(-9));
+    }
+
 }
