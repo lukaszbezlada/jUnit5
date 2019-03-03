e514abb48a4dc2fa8f640a177ea61d5ad7644519
Lukasz Bezlada <bezi@o2.pl>
2019-03-03 17:25:08 +0100

    BeforeAll annotation (must be static)

--- src/test/java/com/infoshareacademy/junit/task1/RangeTest.java
+++ src/test/java/com/infoshareacademy/junit/task1/RangeTest.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.junit.task1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class RangeTest {
 
     private Range range;
+
+    @BeforeAll
+    public static void initAll() {
+        System.out.println("BeforeAll");
+    }
 
     @BeforeEach
     public void initRange() {
